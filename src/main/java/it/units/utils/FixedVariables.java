f03d272118a7c03db9f1329173c562031d7b5f91
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-03-19 17:11:06 +0100

    Update FixedVariables.java

--- src/main/java/it/units/utils/FixedVariables.java
+++ src/main/java/it/units/utils/FixedVariables.java
@@ -5,10 +5,11 @@ public class FixedVariables {
     public static final String UPLOADER = "uploader";
     public static final String ADMINISTRATOR = "administrator";
 
-    public static final String HOMEPAGE = "http://localhost:8080/";
-    //TODO: da aggiornare l'home page
-    // TODO: decidere se mettere anche l'indirizzo per il download
+//    public static final String HOMEPAGE = "http://localhost:8080/";
+    public static final String HOMEPAGE = "https://progettomarchetto.oa.r.appspot.com/";
+    //TODO: dati finali
+
     public static final String BASE_IND_DIRECT_DOWNLOAD_FILES = HOMEPAGE + "api/files/downloadDirect";
 
-    public static final boolean debug = true;
+    public static final boolean debug = false;
 }
